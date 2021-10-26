2387f9fb225dc7aa7c38aae68a1f05927f600d56
pauldg <pauldegeest1@gmail.com>
2021-10-26 16:28:44 +0200

    table definition change

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -512,6 +512,7 @@ class User(Base, Dictifiable, RepresentById):
         cascade='all, delete-orphan',
         collection_class=ordering_list('order_index'))
     _preferences = relationship('UserPreference', collection_class=attribute_mapped_collection('name'))
+    favorite_extensions=relationship('UserFavoriteExtension')
     values = relationship('FormValues',
         primaryjoin=(lambda: User.form_values_id == FormValues.id))  # type: ignore
     # Add type hint (will this work w/SA?)
@@ -8557,6 +8558,12 @@ class UserPreference(Base, RepresentById):
         self.name = name
         self.value = value
 
+class UserFavoriteExtension(Base, RepresentById):
+    __tablename__ = 'user_favorite_extension'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("galaxy_user.id"), index=True)
+    value = Column(Text)
 
 class UserAction(Base, RepresentById):
     __tablename__ = 'user_action'
